e056608e79f903b2102af58175066760b7582eca
licaptio <licaptio42@gmail.com>
2025-11-20 13:51:58 -0600

    Create service-worker.js

---
+++ INSTAPDD/service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "instapdd-cache-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./upload.html",
+  "./viewer.html",
+  "./manifest.json"
+];
+
+// INSTALACIÓN
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+// ACTIVACIÓN
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+// RESPONDER PETICIONES
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
