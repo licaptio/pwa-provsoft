1a714e2f869a9ca46e1827aac5dd3a53feba5bd2
licaptio <licaptio42@gmail.com>
2025-12-07 22:17:48 -0600

    Update pos-firebase.js

--- POSVTA4/js/pos-firebase.js
+++ POSVTA4/js/pos-firebase.js
@@ -24,12 +24,12 @@ const $ = window.$;
 // 🔥 CONFIGURACIÓN FIREBASE
 // -------------------------------
 const firebaseConfig = {
-  apiKey: "AIzaSyD9yXXX",
-  authDomain: "provsoft.firebaseapp.com",
-  projectId: "provsoft",
-  storageBucket: "provsoft.appspot.com",
-  messagingSenderId: "0000000",
-  appId: "1:000000:web:aaaaaa"
+  apiKey: "AIzaSyCK5nb6u2CGRJ8AB1aPlRn54b97bdeAFeM",
+  authDomain: "inventariopv-643f1.firebaseapp.com",
+  projectId: "inventariopv-643f1",
+  storageBucket: "inventariopv-643f1.firebasestorage.app",
+  messagingSenderId: "96242533231",
+  appId: "1:96242533231:web:aae75a18fbaf9840529e9a"
 };
 
 // Inicializar Firebase (solo una vez)
@@ -190,3 +190,4 @@ async function guardarVenta(tipoPago = "EFECTIVO") {
 }
 
 window.guardarVenta = guardarVenta;
+
