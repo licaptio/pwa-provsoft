eae0cfb2d0b0c55ad27f1ec6b2bce3f678b26838
licaptio <licaptio42@gmail.com>
2025-12-07 21:55:29 -0600

    Update pos-firebase.js

--- POSVTA4/js/pos-firebase.js
+++ POSVTA4/js/pos-firebase.js
@@ -192,3 +192,21 @@ export async function guardarVenta(tipoPago = "EFECTIVO") {
 
 // Exponer para otros módulos
 window.guardarVenta = guardarVenta;
+
+// 🔥 Inicialización Firebase
+const firebaseConfig = {
+const firebaseConfig = {
+  apiKey: "AIzaSyCK5nb6u2CGRJ8AB1aPlRn54b97bdeAFeM",
+  authDomain: "inventariopv-643f1.firebaseapp.com",
+  projectId: "inventariopv-643f1",
+  storageBucket: "inventariopv-643f1.firebasestorage.app",
+  messagingSenderId: "96242533231",
+  appId: "1:96242533231:web:aae75a18fbaf9840529e9a"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// 🟦 Exponer Firestore y Auth como GLOBAL
+window.db = firebase.firestore();
+window.auth = firebase.auth();
+window.firebase = firebase;
